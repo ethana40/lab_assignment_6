70e1a526fc2fe79b439905213f3771817eba4927
ethana40 <133877064+ethana40@users.noreply.github.com>
2023-06-23 18:51:48 -0400

    Implemented binary search function

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int search(int numbers[], int low, int high, int value) 
 {
-	return -1;
+	if (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (numbers[mid] == value) {
+            return mid;
+        } else if (numbers[mid] > value) {
+            return search(numbers, low, mid - 1, value);
+        } else {
+            return search(numbers, mid + 1, high, value);
+        }
+    }
+
+    return -1;
+	
 }
 
 void printArray(int numbers[], int sz)
